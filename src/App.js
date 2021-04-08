9136325ad5aa7d89df51af906e2a5e66a61515ee
Bill Spell <william.spell@vanderbilt.edu>
2021-04-07 23:29:34 -0500

    input validation

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         text box. (i.e. 100,200,300)
       </h2>
       <form>
-      <textarea id="uservalues" name="uservalues" rows="4" cols="50" placeholder="Enter Values Here...">
+      <textarea id="uservalues" name="uservalues" rows="4" cols="50" placeholder="Enter Values Here..." onKeyUp={handleInput}>
       </textarea>
       <br/>
       <input type="submit" value="Calculate"/>
       </form>
     </div>
   );
+  function handleInput(e) {
+      e.currentTarget.value = e.currentTarget.value.replace(/[^0-9.,-]/g, '');
+  }
 }
 
 export default App;
