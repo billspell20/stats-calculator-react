9ff162ffabde95a50ba1b047ebd02d09a1fa1dde
Bill Spell <william.spell@vanderbilt.edu>
2021-04-14 00:54:24 -0500

    median

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>William Spell - Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
